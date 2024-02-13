75dab3dcc5adc02f04dfa8c61acdcb9ed89bb8cc
besimorhino <mick.douglas@gmail.com>
2024-02-13 11:59:09 +0000

    sample logging idea

--- loader.py
+++ loader.py
@@ -1,15 +1,32 @@
 from dotenv import load_dotenv
-import os
 from loaders.pdf import LoadPDF
 from loader_functions import Insert, InitCollection
+import os
+import logging
+
+
+### script init elements ###
+logging.basicConfig(filename = './logs/concierge.log',
+                    filemode = 'a', format = '%(asctime)s - %(levelname)s - %(message)s')
 
 load_dotenv()
 
-# variables
-source_path = os.environ['DOCS_DIR']
 
+### hard coded vars ###
+collection_name = "facts"
+
+
+### config loaded variables ###
+if (os.path.isfile('./.env')) and (os.path.exists(os.environ['DOCS_DIR'])):
+    source_path = os.environ['DOCS_DIR']
+else:
+    logging.critical("document directory not set, or path does not exist or is not readable.")
+    exit()
+
+
+### main code ###
 source_files = os.listdir(source_path)
-collection = InitCollection("facts")
+collection = InitCollection(collection_name)
 
 for file in source_files:
     print(file)
